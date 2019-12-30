524759cb32fa21995d172874caeb32df484599ba
Hendi Santika <hendisantika@gmail.com>
2019-12-31 06:56:08 +0700

    #9 - Add JpaConfig

---
+++ src/main/java/com/hendisantika/customermanager/config/JpaConfig.java
@@ -0,0 +1,40 @@
+package com.hendisantika.customermanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : CustomerManager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/19
+ * Time: 06.55
+ */
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.hendisantika.customermanager.entity"})
+@EnableTransactionManagement
+public class JpaConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+        factoryBean.setPersistenceUnitName("SalesDB");
+
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return transactionManager;
+    }
+}
