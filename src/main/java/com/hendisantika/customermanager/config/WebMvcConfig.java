b8837c609045bc06db103b70ca1c624c57391193
Hendi Santika <hendisantika@gmail.com>
2019-12-31 06:50:42 +0700

    #6 - Add WebMvcConfig

---
+++ src/main/java/com/hendisantika/customermanager/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.hendisantika.customermanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : CustomerManager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/12/19
+ * Time: 06.49
+ */
+@Configuration
+@ComponentScan("com.hendisantika.customermanager")
+public class WebMvcConfig {
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
